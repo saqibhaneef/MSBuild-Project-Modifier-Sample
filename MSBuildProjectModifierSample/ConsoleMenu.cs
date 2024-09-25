3a879892a2376557e8b6dbed487a564d47462710
saqibhaneef <muhammadsaqib.ms228@gmail.com>
2024-09-25 16:00:22 +0500

    init

---
+++ MSBuildProjectModifierSample/ConsoleMenu.cs
@@ -0,0 +1,34 @@
+﻿
+
+using MSBuildProjectModifier.Managers;
+
+
+namespace MSBuildProjectModifierSample
+{
+    internal class ConsoleMenu
+    {
+        private readonly IMsBuildManager msBuildManager;
+
+        public ConsoleMenu(IMsBuildManager msBuildManager)
+        {
+            this.msBuildManager = msBuildManager;
+        }
+        public void ProjectModifier()
+        {
+            string projectPath = "D:\\Projects\\Sampl1\\Sample1.csproj";
+            string reference = "Images\\Image.png";
+            
+            bool isProjectExists=this.msBuildManager.IsProjectExists(projectPath);
+
+            if (isProjectExists)
+            {
+                bool isReferenceExists = this.msBuildManager.IsReferenceExists(projectPath, reference);
+                if (isReferenceExists)
+                {
+                    bool isReferenceAdded = this.msBuildManager.AddReference(projectPath, "Images\\Image.png");
+                }
+            }
+
+        }        
+    }
+}
---
+++ MSBuildProjectModifierSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Build.Locator;
+using Microsoft.Extensions.DependencyInjection;
+using MSBuildProjectModifier.Extensions;
+using MSBuildProjectModifierSample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MSBuildLocator.RegisterDefaults();
+       
+        var services = new ServiceCollection();
+        services.BuildServiceProvider();
+        services.AddMsBuildManager();
+
+        services.AddTransient<ConsoleMenu>();
+
+        
+        var serviceProvider = services.BuildServiceProvider();
+        
+
+        var consoleMenu = serviceProvider.GetService<ConsoleMenu>();
+
+        consoleMenu.ProjectModifier();
+
+    }
+
+}
